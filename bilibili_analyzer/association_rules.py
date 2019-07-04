f520081cfab7ae9b0fa9eeee13615f533eb6071c
JachinLi <15650757397@163.com>
2019-07-04 21:35:13 +0800

    展开关联规则后项

--- bilibili_analyzer/association_rules.py
+++ bilibili_analyzer/association_rules.py
@@ -13,20 +13,23 @@ data=pd.read_json(filepath,lines=True)
 data_list = list(data["value"].dropna())
 print(len(data_list))
 
-frequent_itemsets = fpg.find_frequent_itemsets(data_list, minimum_support=0.07 * len(data_list), include_support=True)
-print(type(frequent_itemsets))  # print type
-result = []
-for itemset, support in frequent_itemsets:  # 将generator结果存入list
-    result.append((itemset, support / len(data_list)))
+def generate_frequent_items(minimum_support) :
+    frequent_itemsets = fpg.find_frequent_itemsets(data_list, minimum_support=0.07 * len(data_list), include_support=True)
+    print(type(frequent_itemsets))  # print type
+    result = []
+    for itemset, support in frequent_itemsets:  # 将generator结果存入list
+        result.append((itemset, support / len(data_list)))
 
-result_patterns = [i[0] for i in result]
-result_support = [i[1] for i in result]
-patterns_df = pd.DataFrame({"fluent_patterns": result_patterns, "support": result_support})
-patterns = {}
-for i in result:
-    patterns[frozenset(sorted(i[0]))] = i[1]
-print("-------------挖掘频繁项集---------------")
-print(patterns_df)
+    result_patterns = [i[0] for i in result]
+    result_support = [i[1] for i in result]
+    patterns_df = pd.DataFrame({"fluent_patterns": result_patterns, "support": result_support})
+    patterns = {}
+    for i in result:
+        patterns[frozenset(sorted(i[0]))] = i[1]
+    print("-------------挖掘频繁项集---------------")
+    print(patterns_df)
+
+    return patterns
 
 
 def generate_rules(patterns, min_confidence):
@@ -65,11 +68,49 @@ def transform(raw_rules):
             result.append((base_set, predict_set, confidence))
 
     return result
-raw_rules = generate_rules(patterns, 0.6)
-rules = transform(raw_rules)
-rules.sort(key=lambda x: x[2], reverse=True)
-rules_a = [i[0] for i in rules]
-rules_b = [i[1] for i in rules]
-confidence = [i[2] for i in rules]
-rules_df = pd.DataFrame({"rules_a": rules_a, "rules_b": rules_b, "confidence": confidence})
+
+
+
+
+def transform_rules_to_df(raw_rules) :
+    rules = transform(raw_rules)
+    rules.sort(key=lambda x: x[2], reverse=True)
+    rules_a = [i[0] for i in rules]
+    rules_b = [i[1] for i in rules]
+    confidence = [i[2] for i in rules]
+    rules_df = pd.DataFrame({"rules_a": rules_a, "rules_b": rules_b, "confidence": confidence})
+    print(rules_df)
+
+    return rules_df
+
+
+def unfold_rules(rules_df) :
+    for index, row in rules_df.iterrows():
+        if len(row["rules_b"]) >1 :
+            rules_a = row["rules_a"]
+            rules_b = row["rules_b"]
+            confidence = row["confidence"]
+            rules_df.drop(index=index,inplace=True)
+            for i in rules_b :
+                rules_df = rules_df.append({"rules_a":rules_a, "rules_b":i, "confidence":confidence}, ignore_index=True)
+
+    return rules_df
+
+def count_repeat(rules_df):
+    count = 0
+    for index,row in rules_df.iterrows() :
+        if len(row["rules_b"]) > 1:
+            count +=1
+    print(count)
+
+minimum_support = 0.07
+minimum_confidence = 0.6
+patterns = generate_frequent_items(minimum_support)
+raw_rules = generate_rules(patterns, minimum_confidence)
+rules_df = transform_rules_to_df(raw_rules)
+
+
+count_repeat(rules_df)
+
+rules_df = unfold_rules(rules_df)
 print(rules_df)
